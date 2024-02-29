54cdee36830639b1f2f875c727c5413fdb299a49
EddyShimwa <shedpray16@gmail.com>
2024-02-29 09:58:17 +0200

    add the container for crud operations

--- admin.js
+++ admin.js
@@ -1,24 +1,36 @@
 
-// document.addEventListener("DOMContentLoaded", () => {
-//     const modal = document.getElementById("blogModal");
-//     const btn = document.getElementById("createBlog");
-//     const span = document.getElementsByClassName("close-button")[0];
+document.addEventListener("DOMContentLoaded", () => {
+    const modal = document.getElementById("blogModal");
+    const btn = document.getElementById("createBlog");
+    const closeBtn = document.getElementById("closeModal");
+    const blogList = document.querySelector('.blog-list-container')  
+    const viewBlogs = document.getElementById("viewBlogs");
 
-//     btn.onclick = () => {
-//         modal.style.display = "block";
-//         document.body.style.overflow = "hidden";
-//     };
+    blogList.style.display = "none";
 
-//     span.onclick = () => {
-//         modal.style.display = "none";
-//         document.body.style.overflow = "auto";
-//     };
+    viewBlogs.onclick = () => {
+        if (blogList.style.display === "none") {
+            blogList.style.display = "block";
+        } else {
+            blogList.style.display = "none";
+        }
+    }
+    btn.onclick = () => {
+        modal.style.display = "block";
+        document.body.style.overflow = "hidden";
+    };
 
-//     window.onclick = (event) => {
-//         if (event.target == modal) {
-//             modal.style.display = "none";
-//             document.body.style.overflow = "auto"; 
-//         }
-//     };
-// });
+    closeBtn.onclick = () => {
+        modal.style.display = "none";
+        console.log("close button clicked")
+        document.body.style.overflow = "auto";
+    };
+
+    window.onclick = (event) => {
+        if (event.target == modal) {
+            modal.style.display = "none";
+            document.body.style.overflow = "auto"; 
+        }
+    };
+});
 
